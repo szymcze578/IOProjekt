9a8d3c39a6dce1ba150ffe09c7bce91f0fae0ad9
Szymon Czech <szymcze578@student.polsl.pl>
2023-01-08 12:21:02 +0100

    First commit

--- app/src/main/java/com/example/wypoyczalnia/Menu.java
+++ app/src/main/java/com/example/wypoyczalnia/Menu.java
@@ -1,10 +1,15 @@
 package com.example.wypoyczalnia;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class Menu extends AppCompatActivity {
 
@@ -12,7 +17,16 @@ public class Menu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        NavigationView navigationView = findViewById(R.id.navigationView);
+        navigationView.setItemIconTintList(null);
     }
+
+    public void openNavigationMenu(View view){
+        final DrawerLayout navigation = findViewById(R.id.drawerLayout);
+        navigation.openDrawer(GravityCompat.START);
+    }
+
 
     public void openMapActivity(View view){
 
---
+++ app/src/main/java/com/example/wypoyczalnia/technician_menu.java
@@ -0,0 +1,14 @@
+package com.example.wypoyczalnia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class technician_menu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_technician_menu);
+    }
+}
