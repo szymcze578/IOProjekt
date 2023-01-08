8a337cd3ec74711400e9618ae8dea88e44a8699b
krysnap984 <krysnap984@gmail.com>
2023-01-08 14:49:38 +0100

    Login and registration

--- app/src/main/java/com/example/wypoyczalnia/MainActivity.java
+++ app/src/main/java/com/example/wypoyczalnia/MainActivity.java
@@ -1,14 +1,17 @@
 package com.example.wypoyczalnia;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import android.os.StrictMode;
-
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -16,6 +19,9 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText username, password;
+    Button login, register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,15 +32,72 @@ public class MainActivity extends AppCompatActivity {
         ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.INTERNET}, PackageManager.PERMISSION_GRANTED);
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
-        }
 
-    public void openMainMenuActivity(View view){
-        Intent intent = new Intent(this, Menu.class);
-        startActivity(intent);
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        login = findViewById(R.id.loginBTN);
+        register = findViewById(R.id.signupBTN);
+
+        Connection con = null;
+        final ResultSet[] rs = {null};
+        final PreparedStatement[] pst = {null};
+        con = Database.mycon();
+
+
+        Connection finalCon = con;
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String us = username.getText().toString();
+                String ps = password.getText().toString();
+
+                if(us.equals("") || ps.equals("")){
+                    Toast.makeText(MainActivity.this,"Please enter username and password!",Toast.LENGTH_SHORT).show();
+                }else{
+                    try {
+
+                        String sql = "SELECT * FROM klient WHERE nazwa_uzytkownika=? AND haslo=?";
+                        pst[0] = finalCon.prepareCall(sql);
+                        pst[0].setString(1,us); //user
+                        pst[0].setString(2, ps);   //password
+
+                        rs[0] = pst[0].executeQuery();
+                        if(rs[0].next()){
+                            Toast.makeText(MainActivity.this,"your login.....",Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(MainActivity.this, Menu.class);
+                            startActivity(intent);
+                        }
+                        else {
+                            Toast.makeText(MainActivity.this,"your login failed",Toast.LENGTH_SHORT).show();
+                        }
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+            }
+
+        });
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,Register.class);
+                startActivity(intent);
+            }
+        });
     }
 
-    public void openRegisterActivity(View view){
-        Intent intent = new Intent(this, Register.class);
-        startActivity(intent);
-    }
+
+    //Do usuniecia (prawdopodobnie)
+    //public void openMainMenuActivity(View view){
+    //     Intent intent = new Intent(this, Menu.class);
+    //       startActivity(intent);
+    //  }
+
+    // Do usuniecia prawdopodobnie
+    //  public void openRegisterActivity(View view){
+    //       Intent intent = new Intent(this, Register.class);
+    //      startActivity(intent);
 }
+//}
+
--- app/src/main/java/com/example/wypoyczalnia/Register.java
+++ app/src/main/java/com/example/wypoyczalnia/Register.java
@@ -1,17 +1,66 @@
 package com.example.wypoyczalnia;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.sql.Connection;
+import java.sql.Statement;
 
 public class Register extends AppCompatActivity {
+
+    EditText username, E_mail, password;
+    Button register;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+
+        username = findViewById(R.id.username);
+        E_mail = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        register = findViewById(R.id.signupBTN);
+
+        Connection con = null;
+        con = Database.mycon();
+        Connection finalCon = con;
+
+
+        Connection finalCon1 = con;
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String uname = username.getText().toString();
+                String email = E_mail.getText().toString();
+                String pass = password.getText().toString();
+
+                if(email.equals("")||pass.equals("")||uname.equals("")){
+                    Toast.makeText(Register.this,"Please enter all information!",Toast.LENGTH_SHORT).show();
+                }else{
+                    try{
+
+                        final Statement s = finalCon1.createStatement();
+                        s.executeUpdate("INSERT INTO klient (adres_email,haslo,nazwa_uzytkownika) VALUES ('"+email+"','"+pass+"','"+uname+"') ");
+
+                        Toast.makeText(Register.this,"Your account created succesfully. Log in Now",Toast.LENGTH_SHORT).show();
+
+                    }catch (Exception e){
+                        System.out.println(e);
+                    }
+                }
+                username.setText("");
+                E_mail.setText("");
+                password.setText("");
+            }
+        });
+
     }
 
     public void openLoginActivity(View view){
